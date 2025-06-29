abd8e8571aa5afa966662f2de3b28108dd0da3f8
ngocnghia81 <ngocnghia.workspace@gmail.com>
2025-06-29 21:18:26 +0700

    Replace old project with new Spring Boot project

---
+++ src/main/java/com/group3/MockProject/MockProject062025Group03Application.java
@@ -0,0 +1,12 @@
+package com.group3.MockProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockProject062025Group03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockProject062025Group03Application.class, args);
+	}
+}
---
+++ src/test/java/com/group3/MockProject/MockProject062025Group03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.MockProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockProject062025Group03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
