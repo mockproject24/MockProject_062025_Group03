038c26e3ca619bbf433a1798dc353de64637f96d
Minh <doanminhyb04@gmail.com>
2025-07-01 16:14:46 +0700

    add commment, fix variable naming convention

--- src/main/java/com/group3/MockProject/entity/Question.java
+++ src/main/java/com/group3/MockProject/entity/Question.java
@@ -5,36 +5,53 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.annotations.ColumnDefault;
 
-import java.time.LocalDateTime;
+/**
+ * Question
+ *
+ * Question entity
+ *
+ * Version 1.0
+ * Date: 01/07/2025
+ *
+ * Copyright
+ *
+ * Modification Logs:
+ * DATE         AUTHOR       DESCRIPTION
+ * -------------------------------------
+ * 01/07/2025     DQMinh      Create
+ */
 
 @Entity
 @Data
+@Table(name="question")
 public class Question {
     @Id
-
+    @Column(name="question_id")
     private String questionId;
 
     @NotNull
-    @Column(columnDefinition = "TEXT")
+    @Column(columnDefinition = "TEXT", name="content")
     private String content;
 
     @NotNull
-    @Column(columnDefinition = "MEDIUMTEXT")
+    @Column(columnDefinition = "MEDIUMTEXT", name = "answer")
     private String answer;
 
     @NotNull
+    @Column(name="reliability")
     private String reliability;
 
     @NotNull
     @ColumnDefault("false")
-    private Boolean is_deleted;
+    @Column(name="is_deleted")
+    private Boolean idDeleted;
 
-    // n-1
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="created_by", referencedColumnName = "username")
     private User createdBy;
 
-    // n-1
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="interview_id", referencedColumnName = "interview_id")
     private Interview interview;
